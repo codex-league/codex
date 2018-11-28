57ef7adbc526d02c486b71d9737a087dc6553772
xuchao <xuchao@wondersgroup.com>
2018-11-28 11:18:25 +0800

    xc

---
+++ codex-apix/src/main/java/org/codex/apix/build/ApiListingBuilder.java
@@ -0,0 +1,31 @@
+package org.codex.apix.build;
+
+import org.codex.apix.schema.ApiDescription;
+import org.codex.apix.schema.ApiListing;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+public class ApiListingBuilder {
+
+    private List<ApiDescription> apis = newArrayList();
+
+
+    public static ApiListingBuilder getBuild() {
+        return new ApiListingBuilder();
+    }
+
+    public ApiListingBuilder apis(List<ApiDescription> apis) {
+        if (apis != null) {
+            this.apis = apis;
+        }
+        return this;
+    }
+
+
+
+    public ApiListing build() {
+        return new ApiListing(apis);
+    }
+}
---
+++ codex-apix/src/main/java/org/codex/apix/build/DocumentationBuilder.java
@@ -0,0 +1,27 @@
+package org.codex.apix.build;
+
+import com.google.common.collect.Multimap;
+import org.codex.apix.schema.ApiListing;
+import org.codex.apix.schema.Documentation;
+
+public class DocumentationBuilder {
+
+    private Multimap<String, ApiListing> apiListings;
+
+    public DocumentationBuilder setApiListings(Multimap<String, ApiListing> apiListings) {
+        this.apiListings = apiListings;
+        return this;
+    }
+
+    public static DocumentationBuilder getBuild(){
+        return new DocumentationBuilder();
+    }
+
+    public Documentation build() {
+        return new Documentation(
+                apiListings);
+    }
+
+
+
+}
