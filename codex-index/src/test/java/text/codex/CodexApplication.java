4b7cccb75d135f6bae282f0b23dfb5b66aaef7a1
wcc <wangchengcheng@wondersgroup.com>
2018-11-30 09:40:45 +0800

    随便测试，不重要修改

--- codex-index/src/test/java/text/codex/CodexApplication.java
+++ codex-index/src/test/java/text/codex/CodexApplication.java
@@ -1,8 +1,14 @@
 package text.codex;
 
+<<<<<<< HEAD:codex-index/src/test/java/text/codex/CodexApplication.java
 import pub.codex.index.EnableCodexLeague;
+=======
+import org.codex.core.CodexAutoConfigProvider;
+import org.codex.index.EnableCodexLeague;
+>>>>>>> 随便测试，不重要修改:codex-index/src/test/java/org/codex/CodexApplication.java
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
