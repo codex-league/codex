c0f84daf94ef0584b9a2046dd5584b56924b71f1
wcc <wangchengcheng@wondersgroup.com>
2018-12-03 14:37:42 +0800

    解决冲突

--- codex-index/src/test/java/text/codex/CodexApplication.java
+++ codex-index/src/test/java/text/codex/CodexApplication.java
@@ -1,11 +1,11 @@
 package text.codex;
 
-<<<<<<< HEAD:codex-index/src/test/java/text/codex/CodexApplication.java
+
 import pub.codex.index.EnableCodexLeague;
-=======
-import org.codex.core.CodexAutoConfigProvider;
-import org.codex.index.EnableCodexLeague;
->>>>>>> 随便测试，不重要修改:codex-index/src/test/java/org/codex/CodexApplication.java
+
+import pub.codex.core.CodexAutoConfigProvider;
+import pub.codex.index.EnableCodexLeague;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
