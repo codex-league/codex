de8cd28a1be3cb494c49f4f596ac80ed02a1b034
西 <461720498@qq.com>
2018-12-04 09:42:57 +0800

    CodexLeagueConfig增加配置

---
+++ codex-index/src/main/java/pub/codex/index/CodexLeagueConfig.java
@@ -0,0 +1,11 @@
+package pub.codex.index;
+
+import org.springframework.context.annotation.Configuration;
+import pub.codex.apix.EnableApix;
+import pub.codex.core.EnableCodex;
+
+@EnableApix
+@EnableCodex
+@Configuration
+public class CodexLeagueConfig {
+}
--- codex-index/src/main/java/pub/codex/index/EnableCodexLeague.java
+++ codex-index/src/main/java/pub/codex/index/EnableCodexLeague.java
@@ -1,14 +1,12 @@
 package pub.codex.index;
 
-import pub.codex.apix.EnableApix;
-import pub.codex.core.EnableCodex;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
-@Target(value = { java.lang.annotation.ElementType.TYPE })
-@EnableApix
-@EnableCodex
+@Target(value = {java.lang.annotation.ElementType.TYPE})
+@Import(CodexLeagueConfig.class)
 public @interface EnableCodexLeague {
 }
