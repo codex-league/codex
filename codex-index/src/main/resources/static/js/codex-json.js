1b1e206b476b973ce6ef741ced7fa4f6ac2513a8
xuchao <xuchao@wondersgroup.com>
2018-12-13 17:36:24 +0800

    json格式化，modify bug

--- codex-index/src/main/resources/static/js/codex-json.js
+++ codex-index/src/main/resources/static/js/codex-json.js
@@ -8,178 +8,72 @@ var codex = new Vue({
         },
 
         methods: {
-
-
             //格式化json数据
             formatData(formName) {
                 var _this = this;
 
-                // SetTab();
-                // window.IsCollapsible = $id("CollapsibleView").checked;
-                var json = _this.content;
-                alert(json);
+                var formatJson = function (json, options) {
+                    var reg = null,
+                        formatted = '',
+                        pad = 0,
+                        PADDING = '    ';
+                    options = options || {};
+                    options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
+                    options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
+                    if (typeof json !== 'string') {
+                        json = JSON.stringify(json);
+                    } else {
+                        json = JSON.parse(json);
+                        json = JSON.stringify(json);
+                    }
+                    reg = /([\{\}])/g;
+                    json = json.replace(reg, '\r\n$1\r\n');
+                    reg = /([\[\]])/g;
+                    json = json.replace(reg, '\r\n$1\r\n');
+                    reg = /(\,)/g;
+                    json = json.replace(reg, '$1\r\n');
+                    reg = /(\r\n\r\n)/g;
+                    json = json.replace(reg, '\r\n');
+                    reg = /\r\n\,/g;
+                    json = json.replace(reg, ',');
+                    if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
+                        reg = /\:\r\n\{/g;
+                        json = json.replace(reg, ':{');
+                        reg = /\:\r\n\[/g;
+                        json = json.replace(reg, ':[');
+                    }
+                    if (options.spaceAfterColon) {
+                        reg = /\:/g;
+                        json = json.replace(reg, ':');
+                    }
+                    (json.split('\r\n')).forEach(function (node, index) {
+                            var i = 0,
+                                indent = 0,
+                                padding = '';
 
-                // var html = "";
-                // try {
-                //     if (json == "") {
-                //         json = "\"\"";
-                //         return;
-                //     }
-                //     var obj = eval("[" + json + "]");
-                //     // alert(obj);
-                //
-                //     // html = ProcessObject(obj[0], 0, false, false, false);
-                //     // $id("Canvas").innerHTML = "<PRE class='CodeContainer'>" + html + "</PRE>";
-                // } catch (e) {
-                //     alert("JSON数据格式不正确:\n" + e.message);
-                //     $id("Canvas").innerHTML = "";
-                // }
+                            if (node.match(/\{$/) || node.match(/\[$/)) {
+                                indent = 1;
+                            } else if (node.match(/\}/) || node.match(/\]/)) {
+                                if (pad !== 0) {
+                                    pad -= 1;
+                                }
+                            } else {
+                                indent = 0;
+                            }
 
+                            for (i = 0; i < pad; i++) {
+                                padding += PADDING;
+                            }
 
+                            formatted += padding + node + '\r\n';
+                            pad += indent;
+                        }
+                    );
+                    return formatted;
+                }
+                var resultJson = formatJson(_this.content);
+                _this.content= resultJson ;
             },
-
-            // IsArray(obj) {
-            //     return obj &&
-            //         typeof obj === 'object' &&
-            //         typeof obj.length === 'number' &&
-            //         !(obj.propertyIsEnumerable('length'));
-            // },
-
-            // GetRow(indent, data, isPropertyContent) {
-            //
-            //     var tabs = "";
-            //
-            //     for (var i = 0; i < indent && !isPropertyContent; i++) tabs += window.TAB;
-            //
-            //     if (data != null && data.length > 0 && data.charAt(data.length - 1) != "\n")
-            //
-            //         data = data + "\n";
-            //
-            //     return tabs + data;
-            //
-            // },
-
-            // ProcessObject(obj, indent, addComma, isArray, isPropertyContent) {
-            //     var html = "";
-            //     var comma = (addComma) ? "<span class='Comma'>,</span> " : "";
-            //     var type = typeof obj;
-            //     var clpsHtml = "";
-            //     if (IsArray(obj)) {
-            //         if (obj.length == 0) {
-            //             html += GetRow(indent, "<span class='ArrayBrace'>[ ]</span>" + comma, isPropertyContent);
-            //         } else {
-            //             clpsHtml = window.IsCollapsible ? "<span><img src=\"" + window.ImgExpanded + "\" onClick=\"ExpImgClicked(this)\" /></span><span class='collapsible'>" : "";
-            //             html += GetRow(indent, "<span class='ArrayBrace'>[</span>" + clpsHtml, isPropertyContent);
-            //             for (var i = 0; i < obj.length; i++) {
-            //                 html += ProcessObject(obj[i], indent + 1, i < (obj.length - 1), true, false);
-            //             }
-            //             clpsHtml = window.IsCollapsible ? "</span>" : "";
-            //             html += GetRow(indent, clpsHtml + "<span class='ArrayBrace'>]</span>" + comma);
-            //         }
-            //     } else if (type == 'object') {
-            //         if (obj == null) {
-            //             html += FormatLiteral("null", "", comma, indent, isArray, "Null");
-            //         } else if (obj.constructor == window._dateObj.constructor) {
-            //             html += FormatLiteral("new Date(" + obj.getTime() + ") /*" + obj.toLocaleString() + "*/", "", comma, indent, isArray, "Date");
-            //         } else if (obj.constructor == window._regexpObj.constructor) {
-            //             html += FormatLiteral("new RegExp(" + obj + ")", "", comma, indent, isArray, "RegExp");
-            //         } else {
-            //             var numProps = 0;
-            //             for (var prop in obj) numProps++;
-            //             if (numProps == 0) {
-            //                 html += GetRow(indent, "<span class='ObjectBrace'>{ }</span>" + comma, isPropertyContent);
-            //             } else {
-            //                 clpsHtml = window.IsCollapsible ? "<span><img src=\"" + window.ImgExpanded + "\" onClick=\"ExpImgClicked(this)\" /></span><span class='collapsible'>" : "";
-            //                 html += GetRow(indent, "<span class='ObjectBrace'>{</span>" + clpsHtml, isPropertyContent);
-            //
-            //                 var j = 0;
-            //
-            //                 for (var prop in obj) {
-            //
-            //                     var quote = window.QuoteKeys ? "\"" : "";
-            //
-            //                     html += GetRow(indent + 1, "<span class='PropertyName'>" + quote + prop + quote + "</span>: " + ProcessObject(obj[prop], indent + 1, ++j < numProps, false, true));
-            //
-            //                 }
-            //
-            //                 clpsHtml = window.IsCollapsible ? "</span>" : "";
-            //
-            //                 html += GetRow(indent, clpsHtml + "<span class='ObjectBrace'>}</span>" + comma);
-            //
-            //             }
-            //
-            //         }
-            //
-            //     } else if (type == 'number') {
-            //
-            //         html += FormatLiteral(obj, "", comma, indent, isArray, "Number");
-            //
-            //     } else if (type == 'boolean') {
-            //
-            //         html += FormatLiteral(obj, "", comma, indent, isArray, "Boolean");
-            //
-            //     } else if (type == 'function') {
-            //
-            //         if (obj.constructor == window._regexpObj.constructor) {
-            //
-            //             html += FormatLiteral("new RegExp(" + obj + ")", "", comma, indent, isArray, "RegExp");
-            //
-            //         } else {
-            //
-            //             obj = FormatFunction(indent, obj);
-            //
-            //             html += FormatLiteral(obj, "", comma, indent, isArray, "Function");
-            //
-            //         }
-            //
-            //     } else if (type == 'undefined') {
-            //
-            //         html += FormatLiteral("undefined", "", comma, indent, isArray, "Null");
-            //
-            //     } else {
-            //
-            //         html += FormatLiteral(obj.toString().split("\\").join("\\\\").split('"').join('\\"'), "\"", comma, indent, isArray, "String");
-            //
-            //     }
-            //
-            //     return html;
-            //
-            // },
-
-            // FormatLiteral(literal, quote, comma, indent, isArray, style) {
-            //
-            //     if (typeof literal == 'string')
-            //
-            //         literal = literal.split("<").join("&lt;").split(">").join("&gt;");
-            //
-            //     var str = "<span class='" + style + "'>" + quote + literal + quote + comma + "</span>";
-            //
-            //     if (isArray) str = GetRow(indent, str);
-            //
-            //     return str;
-            //
-            // },
-
-            // FormatFunction(indent, obj) {
-            //
-            //     var tabs = "";
-            //
-            //     for (var i = 0; i < indent; i++) tabs += window.TAB;
-            //
-            //     var funcStrArray = obj.toString().split("\n");
-            //
-            //     var str = "";
-            //
-            //     for (var i = 0; i < funcStrArray.length; i++) {
-            //
-            //         str += ((i == 0) ? "" : tabs) + funcStrArray[i] + "\n";
-            //
-            //     }
-            //
-            //     return str;
-            //
-            // }
-
         }
     })
 ;
